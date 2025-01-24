d6981e5f79ca4e62235ee4292876e528a0062887
UniversiteDounia <douniaaouni123@gmail.com>
2025-01-24 12:02:54 +0100

    Add files via upload

---
+++ codeemetteurcapteur.ino
@@ -0,0 +1,69 @@
+#include <SoftwareSerial.h>
+SoftwareSerial xbee(3, 4);
+int count;
+#include <DHT.h>
+
+
+// Définition des broches et du type de capteur DHT
+#define DHTPIN 2      // Broche de connexion du capteur DHT11
+#define DHTTYPE DHT11 // Type de capteur DHT
+
+
+// Initialisation du capteur DHT
+DHT dht(DHTPIN, DHTTYPE);
+
+
+// Définition de la broche du capteur de lumière
+const int CAPTEUR_LUMIERE = A0;
+
+
+void setup()
+{
+    xbee.begin(9600);                 
+    Serial.begin(9600);                     
+    count = 0;
+// Initialisation de la communication série
+  Serial.begin(9600);
+  Serial.println("Initialisation des capteurs...");
+
+
+  // Démarrage du capteur DHT
+  dht.begin();
+
+
+  // Configuration de la broche pour le capteur de lumière
+  pinMode(CAPTEUR_LUMIERE, INPUT);
+
+}
+ 
+/***************************************
+*  MAIN LOOP
+***************************************/
+ 
+void loop()
+{
+  // Lecture des données du capteur DHT
+  float temperature = dht.readTemperature();
+  float humidite = dht.readHumidity();
+
+
+  // Lecture des données du capteur de lumière
+  int valeurLuminosite = analogRead(CAPTEUR_LUMIERE);
+
+
+
+
+
+
+  // Pause avant la prochaine lecture
+  delay(2000);
+String phrase = "luminositer humidite temperature";
+  Serial.println(phrase);
+  xbee.println(valeurLuminosite);
+  xbee.println(humidite);
+  xbee.println(temperature);
+  delay(300);
+}
+
+
+
